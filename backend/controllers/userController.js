3a1063c55637f7acf66696dd2d0a499c2b151a4f
bravecharm <charmaineselma@gmail.com>
2021-09-30 10:12:58 +0800

    Sec11 L66 Admin Middleware and Get Users as an Admin from the Backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -108,6 +108,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser, updateUserProfile }
+// @desc Get all users
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
 
-// why are we using asyncHandler
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
