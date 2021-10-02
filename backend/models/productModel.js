fc4bcc7493759c94870f8cb4e4b8b18ba95564f1
bravecharm <charmaineselma@gmail.com>
2021-10-02 16:06:47 +0800

    Sec13 L83 Morgan & Create Product Reviews BE

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,6 +5,7 @@ const reviewSchema = mongoose.Schema(
     name: { type: String, required: true },
     rating: { type: Number, required: true }, // individual rating
     comment: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' }, // to associate the user with the review so we can check if a user already made a review
   },
   { timestamps: true }
 )
